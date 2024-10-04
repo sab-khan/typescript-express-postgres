780367bfee8ee7bb985056d03cbb6c72b9a0e544
sab-khan <sabkhan.niazi@gmail.com>
2024-10-04 10:15:46 +0500

    configure jest to work with ts paths

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,13 @@
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { compilerOptions } from './tsconfig.json';
+
 export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['./tests'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
   transform: {
